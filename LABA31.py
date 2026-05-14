69165a8d41ffb1e27404b1c1a2e32a4d80c4ff9d
zwizipocket808 <gv.pa@mail.ru>
2026-05-14 11:57:04 +0500

    Add files via upload

---
+++ LABA31.py
@@ -0,0 +1,39 @@
+﻿# Рекурсивная версия
+def find_recursive(obj, value):
+    def flatten(lst):
+        result = []
+        for item in lst:
+            if isinstance(item, list):
+                result.extend(flatten(item))
+            else:
+                result.append(item)
+        return result
+    flat = flatten(obj)
+    try:
+        return flat.index(value)
+    except ValueError:
+        return None
+
+# Итеративная версия 
+def find_iterative(obj, value):
+    flat = []
+    stack = [obj]
+    while stack:
+        current = stack.pop()
+        if isinstance(current, list):
+            
+            for item in reversed(current):
+                stack.append(item)
+        else:
+            flat.append(current)
+    
+    try:
+        return flat.index(value)
+    except ValueError:
+        return None
+
+
+print(find_recursive([1, 2, [3, 4, [5, [6, [[]]]]], 4], 4))   
+print(find_recursive([1, 2, [3, 4, [5, [6, [[]]]]], 'spam'], 'spam'))  
+print(find_iterative([1, 2, [3, 4, [5, [6, [[]]]]], 4], 4))   
+print(find_iterative([1, 2, [3, 4, [5, [6, [[]]]]], 'spam'], 'spam'))  
---
+++ LABA32.py
@@ -0,0 +1,20 @@
+﻿# Рекурсивная версия
+def recurrence_recursive(u, v, n):
+    if n == 1:
+        return u, v
+    a_prev, b_prev = recurrence_recursive(u, v, n-1)
+    a = 2 * b_prev + a_prev
+    b = 2 * b_prev * b_prev + b_prev   # b_prev^2
+    return a, b
+
+# Итеративная версия
+def recurrence_iterative(u, v, n):
+    a, b = u, v
+    for _ in range(2, n+1):
+        a, b = 2 * b + a, 2 * b * b + b
+    return a, b
+
+
+u, v, n = 1, 2, 3
+print(recurrence_recursive(u, v, n))   
+ 
